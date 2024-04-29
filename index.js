7c02fa08914251fee1eb7f99f6dff692a56f775e
sebrin2445 <153431526+sebrin2445@users.noreply.github.com>
2024-04-29 17:55:25 -0600

    uploaded images and stuffs

--- index.js
+++ index.js
@@ -16,7 +16,30 @@ document.getElementById("btns").onclick = function(){
 
     username=document.getElementById("inputyour").value;
     othername=document.getElementById("inputhis").value;
-if(loveCal===100){
+if(loveCal===100&& loveCal<90){
+    var element=document.getElementById("sec");
+    
+    element.style.display="block";
+    var elementt=document.getElementById("head");
+    elementt.style.display="block";
+    var lii=document.querySelectorAll("#li");
+    var image="images/justgetmarried.png";
+
+    for(var i=0; i<lii.length;i++){
+        lii[i].style.backgroundImage="url('"+image+"')";
+
+        
+    
+    }
+    
+    var lii2=document.querySelectorAll("#li1");
+    var imagee="images/married.png";
+    for(var i=0; i<lii.length;i++){
+        lii2[i].style.backgroundImage="url('"+imagee+"')";
+
+        
+    
+    }
    document.getElementById("form").innerHTML="your love score is "+ loveCal+"%" + " Congratulation your now husband and wife";
 
    document.getElementById("title").innerHTML="your soulmate is "+ othername;
@@ -26,18 +49,33 @@ if(loveCal===100){
     var element=document.getElementById("sec");
     
     element.style.display="block";
+    var elementt=document.getElementById("head");
+    elementt.style.display="block";
     var lii=document.querySelectorAll("#li");
-    var image="images/heart1.png";
+    var image="images/melted.png";
 
     for(var i=0; i<lii.length;i++){
         lii[i].style.backgroundImage="url('"+image+"')";
+
+        
+    
+    }
+    
+    var lii2=document.querySelectorAll("#li1");
+    var imagee="images/melting.png";
+    for(var i=0; i<lii.length;i++){
+        lii2[i].style.backgroundImage="url('"+imagee+"')";
+
+        
     
     }
     
     hey=loveCal + "%";
 document.getElementById("title").innerHTML="I think We got the soulmate "+ othername + " is the one";
 
-document.getElementById("form").innerHTML="Dear " + username + " Your love score is "+ hey;
+var textt=document.getElementById("form").innerHTML="Dear " + username + " Your love score is "+ hey;
+textt.style.fontweight ="800";
+
 document.section.style.display="block";
 }
 else if(loveCal<75 && loveCal>30){
@@ -46,11 +84,24 @@ else if(loveCal<75 && loveCal>30){
     var element=document.getElementById("sec");
     
     element.style.display="block";
+    var elementt=document.getElementById("head");
+    elementt.style.display="block";
     var lii=document.querySelectorAll("#li");
-    var image="images/inlove1.png";
+    var image="images/confusedheart.png";
 
     for(var i=0; i<lii.length;i++){
         lii[i].style.backgroundImage="url('"+image+"')";
+
+        
+    
+    }
+    
+    var lii2=document.querySelectorAll("#li1");
+    var imagee="images/confused.png";
+    for(var i=0; i<lii.length;i++){
+        lii2[i].style.backgroundImage="url('"+imagee+"')";
+
+        
     
     }
     
@@ -59,8 +110,9 @@ else if(loveCal<75 && loveCal>30){
 
 document.getElementById("title").innerHTML="Dear " + username + "Your love score is "+ hey;
 document.body.style.background = "pink";
-document.getElementById("form").innerHTML="Better not to say "+ othername + " is your soulmate";
+var textt=document.getElementById("form").innerHTML="Better not to say "+ othername + " is your soulmate";
 
+textt.style.fontweight ="800";
 
 
 }
@@ -68,18 +120,32 @@ else{
     var element=document.getElementById("sec");
     
     element.style.display="block";
+    var elementt=document.getElementById("head");
+    elementt.style.display="block";
     var lii=document.querySelectorAll("#li");
-    var image="images/brokenheart.jpg";
+    var image="images/brokenheart.png";
 
     for(var i=0; i<lii.length;i++){
         lii[i].style.backgroundImage="url('"+image+"')";
+
+        
+    
+    }
+    
+    var lii2=document.querySelectorAll("#li1");
+    var imagee="images/sad.png";
+    for(var i=0; i<lii.length;i++){
+        lii2[i].style.backgroundImage="url('"+imagee+"')";
+
+        
     
     }
     
     hey=loveCal + "%";
     document.getElementById("title").innerHTML = "Dear " +"  " + username + " this one is hard";
     document.getElementById("imgg").innerHTML = "url('https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2Fid2JtaHR4emk1MXp4d2YzdmV0enJleWVvcjhxem9oYmlnOHpybSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/oPqLjAcwhYosXbTJW7/giphy.gif')";
-    document.getElementById("form").innerHTML="Maybe Your soulmate died idk but ik "+ othername +" will never be ur soulmate sorry dear"
+    var textt=document.getElementById("form").innerHTML="Maybe Your soulmate died idk but ik "+ othername +" will never be ur soulmate sorry dear"
+    textt.style.fontweight ="800";
     document.body.style.backgroundSize = "contain";
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundPosition = "center";
